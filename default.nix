13838afa181af0b344696d398660c8af50bb2f70
Thomas Tuegel <ttuegel@mailbox.org>
2020-04-10 06:39:53 -0500

    Update ghcide and haskell.nix

--- default.nix
+++ default.nix
@@ -1,2 +1,32 @@
-{ pkgs ? import ./nix {} }:
-pkgs.export
+{}:
+let
+  sources = import ./nix/sources.nix;
+  haskellNix = import sources."haskell.nix" {};
+  nixpkgsSrc = haskellNix.sources.nixpkgs-1909;
+  nixpkgsArgs = haskellNix.nixpkgsArgs;
+  pkgs = import nixpkgsSrc nixpkgsArgs;
+  mkPackages = { ghc, stackYaml }:
+    pkgs.haskell-nix.stackProject {
+        src = sources.ghcide;
+        inherit stackYaml;
+        modules = [({config, ...}: {
+          ghc.package = ghc;
+          compiler.version = pkgs.lib.mkForce ghc.version;
+          reinstallableLibGhc = true;
+          packages.ghc.flags.ghci = pkgs.lib.mkForce true;
+          packages.ghci.flags.ghci = pkgs.lib.mkForce true;
+          # This fixes a performance issue, probably https://gitlab.haskell.org/ghc/ghc/issues/15524
+          packages.ghcide.configureFlags = [ "--enable-executable-dynamic" ];
+        })];
+      };
+  mkHieCore = args@{...}:
+    let packages = mkPackages args;
+    in packages.ghcide.components.exes.ghcide // { inherit packages; };
+in
+{
+  # ghcide-ghc881 = mkHieCore { ghc = pkgs.haskell-nix.compiler.ghc881; stackYaml = "stack88.yaml"; };
+  ghcide-ghc865 = mkHieCore { ghc = pkgs.haskell-nix.compiler.ghc865; stackYaml = "stack.yaml"; };
+  ghcide-ghc864 = mkHieCore { ghc = pkgs.haskell-nix.compiler.ghc864; stackYaml = "stack.yaml"; };
+  ghcide-ghc844 = mkHieCore { ghc = pkgs.haskell-nix.compiler.ghc844; stackYaml = "stack84.yaml"; };
+  hie-bios = (mkPackages { ghc = pkgs.haskell-nix.compiler.ghc865; stackYaml = "stack.yaml"; }).hie-bios.components.exes.hie-bios;
+}
